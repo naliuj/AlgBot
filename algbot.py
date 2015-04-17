074b696f0459d43b0789ca0a55d2af633fa1d5e0
Caleb Hoover <choover11@gmail.com>
2015-04-16 21:38:09 -0700

    fleshed out algbot.py, added a function to returns algs from a piece of text and added some comments

--- algbot.py
+++ algbot.py
@@ -8,4 +8,24 @@ def run_bot():
     subreddit = r.get_subreddit('')
     comments = subreddit.get_comments
     for comment in comments:
-        comment_text = comment.body
+        # should I reply to this comment
+        # if yes, reply = writeReply(comment.body)
+        # then continue
+       
+# parses a body of text and returns algs
+def getAlgs(text):
+	while text.find('[') != -1 and text.find(']') != -1: 	# determines that there exists and alg
+		alg = text[text.find('[')+1:text.find(']')] 		# gets that alg
+		text = text.replace('[' + alg + ']', '') 			# removes that piece of text to find next alg
+		yield alg 											# yields that alg and continues to search for more algs
+
+def writeReply(commentBody):
+	replyBody = 'I converted your algs into alg.cubing.net algs. Here there are: '
+	for alg in getAlgs(commentBody):
+		# append to replyBody your link 
+
+	# add signature
+
+# we want to keep scanning
+while True: 
+	run_bot();
