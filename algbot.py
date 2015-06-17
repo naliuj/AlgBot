ddc5a8735bf9598a2edf304db7ad402232694164
naliuj <julianrose@verizon.net>
2015-06-17 09:44:33 -0400

    Tried to do some stuff I guess

--- algbot.py
+++ algbot.py
@@ -4,12 +4,17 @@ import time
 r = praw.Reddit(user_agent= 'alg.cubing.net linker thing')
 r.login()
 
+commentCache = [] # add id's to replied comments here
+
 def run_bot():
     subreddit = r.get_subreddit('')
-    comments = subreddit.get_comments
+    comments = subreddit.get_comments(limit=25)
     for comment in comments:
+    	commentBody=comment.body()
+    	writeReply(commentBody)
         # should I reply to this comment
         # if yes, reply = writeReply(comment.body)
+        # add comment id to commentCache
         # then continue
        
 # parses a body of text and returns algs
@@ -20,7 +25,12 @@ def getAlgs(text):
 		yield alg 											# yields that alg and continues to search for more algs
 
 def writeReply(commentBody):
-	replyBody = 'I converted your algs into alg.cubing.net algs. Here there are: '
+	replyBody = '''Alg: `%`
+
+[alg.cubing.net Link](%)
+
+^I ^am ^a ^bot. ^Message ^the ^moderators ^of ^/r/Cubers ^if ^you ^notice ^any ^issues.''' %alg %alg
+
 	for alg in getAlgs(commentBody):
 		# append to replyBody your link 
 
