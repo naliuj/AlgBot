74cec43de55d883b4bf716e7e656e7e4866e6ff3
naliuj <julianrose@verizon.net>
2015-09-18 08:04:58 -0400

    Made it actually start working.
    It can finally pick out algs, and respond to the comment. There are
    still a few things to work out, but for the most part, it's done.

--- algbot.py
+++ algbot.py
@@ -1,41 +1,36 @@
 import praw
 import time
 
-r = praw.Reddit(user_agent= 'alg.cubing.net linker thing')
-r.login()
+r = praw.Reddit(user_agent= 'alg.cubing.net linker for /r/cubers')
+r.login('AlgBot', )
 
 commentCache = [] # add id's to replied comments here
+blacklist = ['algbot','rubiksbot']
 
 def run_bot():
-    subreddit = r.get_subreddit('')
+    subreddit = r.get_subreddit('naliuj')
     comments = subreddit.get_comments(limit=25)
     for comment in comments:
-    	commentBody=comment.body()
-    	writeReply(commentBody)
-        # should I reply to this comment
-        # if yes, reply = writeReply(comment.body)
-        # add comment id to commentCache
-        # then continue
-       
+        if comment.author.name.lower() not in blacklist:
+            commentBody=comment.body
+            writeReply(commentBody,comment)
+            commentCache.append(comment.id)
+
 # parses a body of text and returns algs
 def getAlgs(text):
-	while text.find('[') != -1 and text.find(']') != -1: 	# determines that there exists and alg
-		alg = text[text.find('[')+1:text.find(']')] 		# gets that alg
-		text = text.replace('[' + alg + ']', '') 			# removes that piece of text to find next alg
-		yield alg 											# yields that alg and continues to search for more algs
+    while text.find('[') != -1 and text.find(']') != -1: 	# determines that there exists and alg
+        alg = text[text.find('[')+1:text.find(']')] 		# gets that alg
+        text = text.replace('[' + alg + ']', '') 			# removes that piece of text to find next alg
+        yield alg 											# yields that alg and continues to search for more algs
 
-def writeReply(commentBody):
-	replyBody = '''Alg: `%`
+def writeReply(commentBody,comment):
+    for alg in getAlgs(commentBody):
+        replyBody ='''Alg: `%s`
 
-[alg.cubing.net Link](%)
-
-^I ^am ^a ^bot. ^Message ^the ^moderators ^of ^/r/Cubers ^if ^you ^notice ^any ^issues.''' %alg %alg
-
-	for alg in getAlgs(commentBody):
-		# append to replyBody your link 
-
-	# add signature
+[alg.cubing.net Link](https://alg.cubing.net/?alg=%s)''' % (alg, alg)
+        comment.reply(replyBody)
 
 # we want to keep scanning
-while True: 
-	run_bot();
+while True:
+    run_bot()
+    time.sleep(10)
