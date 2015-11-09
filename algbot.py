1266bc1ed598abf1bc2a2b301ac1a04c6ba266c0
Caleb Hoover <choover11@gmail.com>
2015-11-09 08:20:09 -0800

    can handle multiple algs now

--- algbot.py
+++ algbot.py
@@ -14,9 +14,8 @@ def run_bot():
     subreddit = r.get_subreddit('naliuj')
     comments = subreddit.get_comments(limit=25)
     for comment in comments:
-        if comment.author.name.lower() not in blacklist:
-            commentBody=comment.body
-            writeReply(commentBody,comment)
+        if comment.author.name.lower() not in blacklist and comment.id not in commentCache:
+            writeReply(comment.body, comment)
             commentCache.append(comment.id)
 
 # parses a body of text and returns algs
@@ -25,20 +24,26 @@ def getAlgs(text):
         alg = text[text.find('[')+1:text.find(']')] 		# gets that alg
         text = text.replace('[' + alg + ']', '') 		# removes that piece of text to find next alg
         if regex.match(alg):
-            print(alg)
             yield alg 						# yields that alg and continues to search for more algs
 
 def writeReply(commentBody,comment):
-    for alg in getAlgs(commentBody):
-        print(alg)
-        replyBody =('''Alg: `%s`
+    algs = [alg for alg in getAlgs(commentBody)]
+    if len(algs) > 0:
+        replyBody = '';
+        for alg in algs:
+            replyBody += ('''Alg: `%s`
 
-[alg.cubing.net Link](https://alg.cubing.net/?alg=%s)
+[alg.cubing.net Link](https://alg.cubing.net/?alg=%s)\n\n''') % (alg, alg)
 
-^^I ^^am ^^a ^^bot. ^^Please ^^Message ^^the ^^moderators ^^of ^^/r/Cubers ^^if ^^there ^^are ^^any ^^issues.''') % (alg, alg)
+        replyBody += '^^I ^^am ^^a ^^bot. ^^Please ^^Message ^^the ^^moderators ^^of ^^/r/Cubers ^^if ^^there ^^are ^^any ^^issues.'
         comment.reply(replyBody)
 
 # we want to keep scanning
 while True:
-    run_bot()
-    time.sleep(10)
+    try:
+        run_bot()
+        time.sleep(10)
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
+        print('error {0}'.format(e))
